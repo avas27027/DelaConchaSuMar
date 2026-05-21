02577fa07b7f00a2e43fdd8a8602d1100b4f8875
avas27027 <alvas27027@gmail.com>
2026-05-20 23:12:22 -0500

    docker prod

--- rest-modules/src/main.ts
+++ rest-modules/src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOrigin = process.env.CORS_ORIGIN?.split(',').map((origin) => origin.trim()).filter(Boolean);
 
   app.enableCors({
-    origin: [
+    origin: corsOrigin?.length ? corsOrigin : [
       'http://localhost:3000',
       'http://127.0.0.1:3000',
       'http://0.0.0.0:3000'
